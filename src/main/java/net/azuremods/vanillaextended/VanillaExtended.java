ca56e619192e2942b793841f8b67e003ca87df15
zianu <hunnybofficial@gmail.com>
2022-07-11 10:23:20 +0300

    Initial commit

---
+++ src/main/java/net/azuremods/vanillaextended/VanillaExtended.java
@@ -0,0 +1,15 @@
+package net.azuremods.vanillaextended;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VanillaExtended implements ModInitializer {
+	public static final String MOD_ID = "vanillaextended";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/azuremods/vanillaextended/VanillaExtendedClient.java
@@ -0,0 +1,10 @@
+package net.azuremods.vanillaextended;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaExtendedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
