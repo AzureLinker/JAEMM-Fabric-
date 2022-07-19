5b7be3a4a3197dc4258bc543b9a88e4ef95c8463
zianu <hunnybofficial@gmail.com>
2022-07-19 04:19:45 +0300

    Fixed Sounds

--- src/main/java/net/azuremods/vanillaextended/block/ModBlocks.java
+++ src/main/java/net/azuremods/vanillaextended/block/ModBlocks.java
@@ -9,6 +9,7 @@ import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -21,7 +22,7 @@ public class ModBlocks {
     public static final Block POLISHED_GRANITE_BRICKS = registerBlock("polished_granite_bricks",
             new Block(FabricBlockSettings.of(Material.STONE).strength(2f).requiresTool()), ModItemGroup.VANILLA_EXTENDED);
     public static final Block BAMBOO_FLOOR = registerBlock("bamboo_floor",
-            new Block(FabricBlockSettings.of(Material.BAMBOO).strength(0.3f)), ModItemGroup.VANILLA_EXTENDED);
+            new Block(FabricBlockSettings.of(Material.BAMBOO).strength(0.3f).sounds(BlockSoundGroup.BAMBOO)), ModItemGroup.VANILLA_EXTENDED);
 
     private static Block registerBlock(String name, Block block, ItemGroup tab){
         registerBlockItem(name, block, tab);
