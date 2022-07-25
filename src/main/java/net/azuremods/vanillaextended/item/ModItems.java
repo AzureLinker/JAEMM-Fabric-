234b5dbeb4e4a25b9f0a8a1ddebbb6071fe59c87
zianu <hunnybofficial@gmail.com>
2022-07-25 13:03:45 +0300

    Changed Name

--- src/main/java/net/azuremods/vanillaextended/item/ModItems.java
+++ src/main/java/net/azuremods/vanillaextended/item/ModItems.java
@@ -37,16 +37,16 @@ public class ModItems {
             new SwordItem(ModToolMaterials.STEEL, 3, -2.3f,
                     new FabricItemSettings().group(ModItemGroup.VANILLA_EXTENDED)));
     public static final Item STEEL_AXE = registerItem("steel_axe",
-            new ModAxeItem(ModToolMaterials.STEEL, 3, -2.9f,
+            new ModAxeItem(ModToolMaterials.STEEL, 6, -2.9f,
                     new FabricItemSettings().group(ModItemGroup.VANILLA_EXTENDED)));
     public static final Item STEEL_HOE = registerItem("steel_hoe",
-            new ModHoeItem(ModToolMaterials.STEEL, 0, -3f,
+            new ModHoeItem(ModToolMaterials.STEEL, -2, 0f,
                     new FabricItemSettings().group(ModItemGroup.VANILLA_EXTENDED)));
     public static final Item STEEL_SHOVEL = registerItem("steel_shovel",
-            new ShovelItem(ModToolMaterials.STEEL, 0, -3f,
+            new ShovelItem(ModToolMaterials.STEEL, -2, -3f,
                     new FabricItemSettings().group(ModItemGroup.VANILLA_EXTENDED)));
     public static final Item STEEL_PICKAXE = registerItem("steel_pickaxe",
-            new ModPickaxeItem(ModToolMaterials.STEEL, 0, -3f,
+            new ModPickaxeItem(ModToolMaterials.STEEL, -1, -3f,
                     new FabricItemSettings().group(ModItemGroup.VANILLA_EXTENDED)));
 
     private static Item registerItem(String name, Item item){
